1e07992ccd364e848a121800516f94e89b572b80
Thibault Guilhem <thibault.guilhem@gmail.com>
2022-12-11 20:43:22 +0100

    pictures.slice

---
+++ src/feature/pictures.slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const picturesSlice = createSlice({
+	name: 'pictures',
+	initialState: {
+		pictures: null,
+	},
+
+	reducers: {
+		setPicturesData: (state, { payload }) => {
+			state.pictures = payload;
+		},
+		addPicture: (state, { payload }) => {
+			state.pictures.push(payload);
+		},
+	},
+});
+
+export const { setPicturesData, addPicture } = picturesSlice.actions;
+export default picturesSlice.reducer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import './styles/index.css';
+import store from './app/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
